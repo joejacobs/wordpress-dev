070ee385f030704e4646219ea95b6bd4aae1224a
Joseph Jacobs <joe@hazardcell.com>
2013-07-28 17:15:21 +0000

    Cleaned up wp-config.php and fixed ABSPATH and WP_CONTENT_URL

--- wp-config.php
+++ wp-config.php
@@ -1,99 +1,52 @@
 <?php
-/**
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
- */
 
 if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 	define( 'WP_LOCAL_DEV', true );
 	include( dirname( __FILE__ ) . '/local-config.php' );
 } else {
-	// ** MySQL settings - You can get this info from your web host ** //
-	/** The name of the database for WordPress */
-	define('DB_NAME', 'database_name_here');
-
-	/** MySQL database username */
-	define('DB_USER', 'username_here');
-
-	/** MySQL database password */
-	define('DB_PASSWORD', 'password_here');
-
-	/** MySQL hostname */
-	define('DB_HOST', 'localhost');
+	define( 'WP_LOCAL_DEV', false );
+	define( 'DB_NAME', 'database_name' );
+	define( 'DB_USER', 'database_username' );
+	define( 'DB_PASSWORD', 'database_password' );
+	define( 'DB_HOST', 'database_host' );
 }
 
-/** Custom content directory */
+// Custom content directory
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
-define( 'WP_CONTENT_URL', 'http://' . $_SERVER[ 'HTTP-HOST' ] . '/content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER[ 'HTTP_HOST' ] . '/content' );
 
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+// Database Charset to use in creating database tables
+define( 'DB_CHARSET', 'utf8' );
 
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+// The Database Collate type. Don't change this if in doubt
+define( 'DB_COLLATE', '' );
 
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+// Salts, https://api.wordpress.org/secret-key/1.1/salt/
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
 
-/**#@-*/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
- */
+// Table prefix
 $table_prefix  = 'wp_';
 
-/**
- * WordPress Localized Language, defaults to English.
- *
- * Change this to localize WordPress. A corresponding MO file for the chosen
- * language must be installed to wp-content/languages. For example, install
- * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
- * language support.
- */
-define('WPLANG', '');
+// Language, defaults to English
+define( 'WPLANG', '' );
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- */
-define('WP_DEBUG', false);
+// Enable for debug mode
+define( 'WP_DEBUG', false );
 
-/* That's all, stop editing! Happy blogging. */
+// Hide errors
+ini_set( 'display_errors', 0 );
+define( 'WP_DEBUG_DISPLAY', false );
 
-/** Absolute path to the WordPress directory. */
+// Absolute path to the WordPress directory
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+	define( 'ABSPATH', dirname(__FILE__) . '/wordpress/' );
 
-/** Sets up WordPress vars and included files. */
+// Sets up WordPress vars and included files
 require_once(ABSPATH . 'wp-settings.php');
